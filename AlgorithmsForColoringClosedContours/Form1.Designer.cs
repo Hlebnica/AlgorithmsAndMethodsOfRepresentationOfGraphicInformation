dd6c77e293662519b4bf2b5fd8738edd808eb525
Hlebnica <apple260500@gmail.com>
2023-11-07 01:14:37 +0500

    coloring end

--- AlgorithmsForColoringClosedContours/Form1.Designer.cs
+++ AlgorithmsForColoringClosedContours/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.buttonSeedFilling = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.buttonColorPicker = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForFigure)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -44,9 +45,9 @@
             this.pictureBoxForFigure.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBoxForFigure.Location = new System.Drawing.Point(214, 18);
+            this.pictureBoxForFigure.Location = new System.Drawing.Point(223, 18);
             this.pictureBoxForFigure.Name = "pictureBoxForFigure";
-            this.pictureBoxForFigure.Size = new System.Drawing.Size(747, 603);
+            this.pictureBoxForFigure.Size = new System.Drawing.Size(738, 603);
             this.pictureBoxForFigure.TabIndex = 2;
             this.pictureBoxForFigure.TabStop = false;
             this.pictureBoxForFigure.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBoxForFigure_MouseClick);
@@ -56,7 +57,7 @@
             this.buttonCreateFigure.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonCreateFigure.Location = new System.Drawing.Point(5, 18);
             this.buttonCreateFigure.Name = "buttonCreateFigure";
-            this.buttonCreateFigure.Size = new System.Drawing.Size(190, 44);
+            this.buttonCreateFigure.Size = new System.Drawing.Size(199, 44);
             this.buttonCreateFigure.TabIndex = 3;
             this.buttonCreateFigure.Text = "Создать фигуру";
             this.buttonCreateFigure.UseVisualStyleBackColor = true;
@@ -66,24 +67,25 @@
             // 
             this.groupBox1.Controls.Add(this.buttonLineByLineFilling);
             this.groupBox1.Controls.Add(this.buttonSeedFilling);
+            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.buttonColorPicker);
             this.groupBox1.Controls.Add(this.buttonCreateFigure);
             this.groupBox1.Location = new System.Drawing.Point(9, 10);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(200, 612);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(209, 612);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
             // buttonLineByLineFilling
             // 
             this.buttonLineByLineFilling.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonLineByLineFilling.Location = new System.Drawing.Point(4, 228);
-            this.buttonLineByLineFilling.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonLineByLineFilling.Location = new System.Drawing.Point(5, 320);
+            this.buttonLineByLineFilling.Margin = new System.Windows.Forms.Padding(2);
             this.buttonLineByLineFilling.Name = "buttonLineByLineFilling";
-            this.buttonLineByLineFilling.Size = new System.Drawing.Size(190, 57);
+            this.buttonLineByLineFilling.Size = new System.Drawing.Size(200, 57);
             this.buttonLineByLineFilling.TabIndex = 7;
             this.buttonLineByLineFilling.Text = "Построчное сканирование";
             this.buttonLineByLineFilling.UseVisualStyleBackColor = true;
@@ -92,10 +94,10 @@
             // buttonSeedFilling
             // 
             this.buttonSeedFilling.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSeedFilling.Location = new System.Drawing.Point(4, 166);
-            this.buttonSeedFilling.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonSeedFilling.Location = new System.Drawing.Point(5, 258);
+            this.buttonSeedFilling.Margin = new System.Windows.Forms.Padding(2);
             this.buttonSeedFilling.Name = "buttonSeedFilling";
-            this.buttonSeedFilling.Size = new System.Drawing.Size(190, 48);
+            this.buttonSeedFilling.Size = new System.Drawing.Size(200, 48);
             this.buttonSeedFilling.TabIndex = 6;
             this.buttonSeedFilling.Text = "Заливка с затравкой";
             this.buttonSeedFilling.UseVisualStyleBackColor = true;
@@ -105,7 +107,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(4, 132);
+            this.label1.Location = new System.Drawing.Point(44, 235);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(123, 21);
@@ -116,13 +118,25 @@
             // 
             this.buttonColorPicker.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonColorPicker.Location = new System.Drawing.Point(4, 67);
-            this.buttonColorPicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonColorPicker.Margin = new System.Windows.Forms.Padding(2);
             this.buttonColorPicker.Name = "buttonColorPicker";
-            this.buttonColorPicker.Size = new System.Drawing.Size(190, 44);
+            this.buttonColorPicker.Size = new System.Drawing.Size(201, 44);
             this.buttonColorPicker.TabIndex = 4;
             this.buttonColorPicker.Text = "Выбрать цвет";
             this.buttonColorPicker.UseVisualStyleBackColor = true;
             this.buttonColorPicker.Click += new System.EventHandler(this.buttonColorPicker_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(4, 131);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 90);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Для начала заливики\r\nнеобходимо выбрать\r\nточку на фигуре\r\nнажатием ЛКМ";
             // 
             // Form1
             // 
@@ -131,7 +145,7 @@
             this.ClientSize = new System.Drawing.Size(971, 632);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBoxForFigure);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Старостенок КЭ-403 Алгоритмы закраски замкнутых контуров";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForFigure)).EndInit();
@@ -150,6 +164,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button buttonLineByLineFilling;
         private System.Windows.Forms.Button buttonSeedFilling;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- AlgorithmsForColoringClosedContours/Form1.cs
+++ AlgorithmsForColoringClosedContours/Form1.cs
@@ -89,40 +89,59 @@ namespace AlgorithmsForColoringClosedContours
             }
         }
 
-        private void buttonSeedFilling_Click(object sender, EventArgs e)
+        private async void buttonSeedFilling_Click(object sender, EventArgs e)
         {
-            int startX = selectedX;
-            int startY = selectedY;
+            int startX = selectedX; 
+            int startY = selectedY; 
 
+            // Проверка: находится ли начальная точка внутри полигона
             if (!IsPointInPolygon(startX, startY, _currentPoints))
             {
-                MessageBox.Show("Выбранная точка не соответствует условиям.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
+                MessageBox.Show("Выберите точку внутри фигуры для начала заливки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return; 
             }
 
-            Color targetColor = _bitmap.GetPixel(startX, startY);
+            Color targetColor = _bitmap.GetPixel(startX, startY); // Получение цвета пикселя в начальной точке
 
-            Stack<Point> pixelsToFill = new Stack<Point>();
-            pixelsToFill.Push(new Point(startX, startY));
+            Stack<Point> pixelsToFill = new Stack<Point>(); // Создание стека для хранения пикселей, которые нужно закрасить
+            pixelsToFill.Push(new Point(startX, startY)); // Добавление начального пикселя в стек
+
+            int filledPixels = 0; // Счетчик закрашенных пикселей
 
             while (pixelsToFill.Count > 0)
             {
-                Point currentPixel = pixelsToFill.Pop();
-                int x = currentPixel.X;
-                int y = currentPixel.Y;
+                Point currentPixel = pixelsToFill.Pop(); // Извлечение пикселя из стека
+                int x = currentPixel.X; // Получение X координаты текущего пикселя
+                int y = currentPixel.Y; // Получение Y координаты текущего пикселя
 
+                // Проверка, находится ли пиксель в пределах изображения и имеет ли он цвет, который нужно заменить
                 if (x >= 0 && x < _bitmap.Width && y >= 0 && y < _bitmap.Height && _bitmap.GetPixel(x, y) == targetColor)
                 {
-                    _bitmap.SetPixel(x, y, _fillColor);
+                    _bitmap.SetPixel(x, y, _fillColor); // Закраска пикселя выбранным цветом
+                    filledPixels++; 
 
-                    pictureBoxForFigure.Refresh();
+                    // Задержка для визуализации процесса
+                    if (filledPixels % 20 == 0)
+                    {
+                        pictureBoxForFigure.Refresh(); 
+                        await Task.Delay(5); 
+                    }
 
-                    pixelsToFill.Push(new Point(x + 1, y));
-                    pixelsToFill.Push(new Point(x - 1, y));
-                    pixelsToFill.Push(new Point(x, y + 1));
-                    pixelsToFill.Push(new Point(x, y - 1));
+                    // Добавление соседних пикселей в стек для дальнейшей заливки
+                    pixelsToFill.Push(new Point(x + 1, y)); // Добавление пикселя справа
+                    pixelsToFill.Push(new Point(x - 1, y)); // Добавление пикселя слева
+                    pixelsToFill.Push(new Point(x, y + 1)); // Добавление пикселя сверху
+                    pixelsToFill.Push(new Point(x, y - 1)); // Добавление пикселя снизу 
+
+                    /*
+                    pixelsToFill.Push(new Point(x, y - 1)); // Добавление пикселя сверху
+                    pixelsToFill.Push(new Point(x, y + 1)); // Добавление пикселя снизу
+                    pixelsToFill.Push(new Point(x - 1, y)); // Добавление пикселя слева
+                    pixelsToFill.Push(new Point(x + 1, y)); // Добавление пикселя справа
+                    */
                 }
             }
+            pictureBoxForFigure.Refresh();
         }
 
         private void pictureBoxForFigure_MouseClick(object sender, MouseEventArgs e)
@@ -133,64 +152,62 @@ namespace AlgorithmsForColoringClosedContours
 
         private bool IsPointInPolygon(int x, int y, List<PointF> polygon)
         {
-            int crossings = 0;
+            int crossings = 0; // Инициализация счетчика пересечений линии из точки с ребрами полигона
+            // Цикл по всем вершинам полигона
             for (int i = 0; i < polygon.Count; i++)
             {
-                PointF p1 = polygon[i];
-                PointF p2 = polygon[(i + 1) % polygon.Count];
+                PointF p1 = polygon[i]; // Текущая вершина полигона
+                PointF p2 = polygon[(i + 1) % polygon.Count]; // Следующая вершина полигона, с учетом замыкания контура
+                // Проверка, пересекает ли горизонтальная линия из точки текущее ребро полигона
                 if (p1.Y <= y && p2.Y > y || p1.Y > y && p2.Y <= y)
                 {
+                    // Вычисление X координаты точки пересечения ребра полигона с горизонтальной линией
                     float vt = (float)(y - p1.Y) / (p2.Y - p1.Y);
+                    // Если X координата точки меньше X координаты найденной точки пересечения, увеличиваем счетчик пересечений
                     if (x < p1.X + vt * (p2.X - p1.X))
                         crossings++;
                 }
             }
+            // Если число пересечений нечетное, точка находится внутри полигона, иначе - снаружи
             return (crossings % 2 == 1);
         }
 
+
         private void buttonLineByLineFilling_Click(object sender, EventArgs e)
         {
-            int startX = selectedX;
-            int startY = selectedY;
-
-            if (startX >= 0 && startX < pictureBoxForFigure.Width && startY >= 0 && startY < pictureBoxForFigure.Height && IsPointInPolygon(startX, startY, _currentPoints))
+            if (selectedX == -1 || selectedY == -1 || !IsPointInPolygon(selectedX, selectedY, _currentPoints))
             {
-                LineByLineFill(startX, startY);
+                MessageBox.Show("Выберите точку внутри фигуры для начала заливки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
-            else
+
+            Color targetColor = _bitmap.GetPixel(selectedX, selectedY); // Получение цвета пикселя в начальной точке
+            Queue<Point> nodes = new Queue<Point>(); // Создание стека для хранения пикселей, которые нужно закрасить
+            nodes.Enqueue(new Point(selectedX, selectedY));
+
+            while (nodes.Count > 0)
             {
-                MessageBox.Show("Выбранная точка не соответствует условиям.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
+                Point node = nodes.Dequeue();
+                int x = node.X;
+                int y = node.Y;
 
-        private async void LineByLineFill(int startX, int startY)
-        {
-            Color targetColor = _bitmap.GetPixel(startX, startY);
-            Stack<Point> stack = new Stack<Point>();
-            stack.Push(new Point(startX, startY));
-
-            while (stack.Count > 0)
-            {
-                Point current = stack.Pop();
-                int x = current.X;
-                int y = current.Y;
-
-                while (x >= 0 && _bitmap.GetPixel(x, y) == targetColor)
+                // Поиск крайней левой точки горизонтальной линии
+                while (x > 0 && _bitmap.GetPixel(x - 1, y) == targetColor)
                 {
                     x--;
                 }
-                x++;
 
                 bool spanAbove = false;
                 bool spanBelow = false;
 
+                // Проход вправо от найденной крайней левой точки, заливка и определение точек для следующих строк
                 while (x < _bitmap.Width && _bitmap.GetPixel(x, y) == targetColor)
                 {
                     _bitmap.SetPixel(x, y, _fillColor);
 
                     if (!spanAbove && y > 0 && _bitmap.GetPixel(x, y - 1) == targetColor)
                     {
-                        stack.Push(new Point(x, y - 1));
+                        nodes.Enqueue(new Point(x, y - 1));
                         spanAbove = true;
                     }
                     else if (spanAbove && y > 0 && _bitmap.GetPixel(x, y - 1) != targetColor)
@@ -200,63 +217,7 @@ namespace AlgorithmsForColoringClosedContours
 
                     if (!spanBelow && y < _bitmap.Height - 1 && _bitmap.GetPixel(x, y + 1) == targetColor)
                     {
-                        stack.Push(new Point(x, y + 1));
-                        spanBelow = true;
-                    }
-                    else if (spanBelow && y < _bitmap.Height - 1 && _bitmap.GetPixel(x, y + 1) != targetColor)
-                    {
-                        spanBelow = false;
-                    }
-
-                    x++;
-
-                    pictureBoxForFigure.Invalidate();
-                    await Task.Delay(1); 
-                }
-            }
-
-            pictureBoxForFigure.Invalidate();
-        }
-
-        /*
-        private void LineByLineFill(int startX, int startY)
-        {
-            Color targetColor = _bitmap.GetPixel(startX, startY);
-            Stack<Point> stack = new Stack<Point>();
-            stack.Push(new Point(startX, startY));
-
-            while (stack.Count > 0)
-            {
-                Point current = stack.Pop();
-                int x = current.X;
-                int y = current.Y;
-
-                while (x >= 0 && _bitmap.GetPixel(x, y) == targetColor)
-                {
-                    x--;
-                }
-                x++; // Восстановите правильное положение
-
-                bool spanAbove = false;
-                bool spanBelow = false;
-
-                while (x < _bitmap.Width && _bitmap.GetPixel(x, y) == targetColor)
-                {
-                    _bitmap.SetPixel(x, y, _fillColor);
-
-                    if (!spanAbove && y > 0 && _bitmap.GetPixel(x, y - 1) == targetColor)
-                    {
-                        stack.Push(new Point(x, y - 1));
-                        spanAbove = true;
-                    }
-                    else if (spanAbove && y > 0 && _bitmap.GetPixel(x, y - 1) != targetColor)
-                    {
-                        spanAbove = false;
-                    }
-
-                    if (!spanBelow && y < _bitmap.Height - 1 && _bitmap.GetPixel(x, y + 1) == targetColor)
-                    {
-                        stack.Push(new Point(x, y + 1));
+                        nodes.Enqueue(new Point(x, y + 1));
                         spanBelow = true;
                     }
                     else if (spanBelow && y < _bitmap.Height - 1 && _bitmap.GetPixel(x, y + 1) != targetColor)
@@ -266,11 +227,10 @@ namespace AlgorithmsForColoringClosedContours
 
                     x++;
                 }
+
+                pictureBoxForFigure.Refresh(); // Обновление изображения для отображения процесса заливки
+                Task.Delay(10).Wait(); // Задержка для визуализации процесса
             }
-
-            pictureBoxForFigure.Invalidate();
         }
-
-        */
     }
 }
