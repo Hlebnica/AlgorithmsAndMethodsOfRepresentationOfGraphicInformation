a81a0c8563728fc5bd80f458d9f2dbe3ec804c0a
Dmitry Starostenok <49001181+Hlebnica@users.noreply.github.com>
2023-11-21 11:15:12 +0500

    start test fractals

---
+++ Fractals/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Fractals
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateFractal = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxIterations = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerateFractal
+            // 
+            this.btnGenerateFractal.Location = new System.Drawing.Point(823, 95);
+            this.btnGenerateFractal.Name = "btnGenerateFractal";
+            this.btnGenerateFractal.Size = new System.Drawing.Size(148, 35);
+            this.btnGenerateFractal.TabIndex = 1;
+            this.btnGenerateFractal.Text = "Сгенерировать";
+            this.btnGenerateFractal.UseVisualStyleBackColor = true;
+            this.btnGenerateFractal.Click += new System.EventHandler(this.btnGenerateFractal_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(805, 730);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(820, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Количество итераций";
+            // 
+            // textBoxIterations
+            // 
+            this.textBoxIterations.Location = new System.Drawing.Point(823, 67);
+            this.textBoxIterations.Name = "textBoxIterations";
+            this.textBoxIterations.Size = new System.Drawing.Size(145, 22);
+            this.textBoxIterations.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(980, 754);
+            this.Controls.Add(this.textBoxIterations);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnGenerateFractal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGenerateFractal;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxIterations;
+    }
+}
+
---
+++ Fractals/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateFractal_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxIterations.Text, out int maxIterations))
+            {
+                double zoom = 150;
+                double offsetX = pictureBox.Width / 2.0 + 100;
+                double offsetY = pictureBox.Height / 2.0;
+
+                Bitmap fractalBitmap = GenerateFractal(maxIterations, zoom, offsetX, offsetY);
+                pictureBox.Image = fractalBitmap;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное число для уровней вложенности.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private Bitmap GenerateFractal(int maxIterations, double zoom, double offsetX, double offsetY)
+        {
+            int width = pictureBox.Width;    
+            int height = pictureBox.Height;  
+
+            Bitmap bitmap = new Bitmap(width, height); 
+
+            // Проход по каждому пикселю изображения
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // Преобразование координат пикселя в координаты фрактала с учетом масштабирования и сдвига
+                    double zx = (x - offsetX) / zoom;
+                    double zy = (y - offsetY) / zoom;
+
+                    double cx = zx;
+                    double cy = zy;
+
+                    int iteration = 0;
+
+                    // Вычисление фрактала с использованием итераций
+                    while (iteration < maxIterations)
+                    {
+                        // Фрактал Мандельброта
+                        //         2
+                        // Z    = Z   + C
+                        //  n+1    n
+                        double newZx = zx * zx - zy * zy + cx;
+                        double newZy = 2 * zx * zy + cy;
+
+                        zx = newZx;
+                        zy = newZy;
+
+                        // Если выходит за пределы установленной границы, завершение итерации
+                        if (zx * zx + zy * zy > 4)
+                            break;
+
+                        iteration++;
+                    }
+
+                    // Определение цвета пикселя на основе числа итераций
+                    Color color = GetColor(iteration, maxIterations);
+
+                    // Установка цвета пикселя в изображении
+                    bitmap.SetPixel(x, y, color);
+                }
+            }
+
+            return bitmap;
+        }
+
+        // Метод GetColor возвращает цвет в зависимости от числа итераций
+        private Color GetColor(int iteration, int maxIterations)
+        {
+            if (iteration == maxIterations)
+            {
+                return Color.Black;  // Возвращаем черный цвет для точек, не выходящих за границы фрактала
+            }
+            else
+            {
+                // Вычисление цвета в зависимости от числа итераций
+                double hue = (double)iteration / maxIterations * 360.0;
+
+                // Цвет, зависящий от числа итераций
+                return Color.FromArgb(255, (int)hue % 256, 0, 0);
+            }
+        }
+
+
+    }
+}
